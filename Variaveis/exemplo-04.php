a7386de84991e32fd2fd4c97da2fc98981025111
Gabriel Teixeira <gabrielgabriel100@hotmail.com>
2018-12-14 21:12:10 -0200

    receber paramentro via url

---
+++ Variaveis/exemplo-04.php
@@ -0,0 +1,10 @@
+<?php
+
+//a variavel "a" irá receber um valor pela url atraves do caracter "?".
+$nome = (int)$_GET["a"]; 
+
+// para passar o valor 
+//http://localhost/Projeto-PHP/Variaveis/exemplo-04.php?a=123
+var_dump($nome);//descobrir espaço e tipo de variavel
+
+?>
